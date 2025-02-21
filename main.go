31f6336878f7409320f8c14f9e3e47f4cf72e449
Muto1907 <Mahmut.Cinar93@outlook.de>
2025-02-21 19:12:57 +0100

    Create consts for parameters and add Prints

--- main.go
+++ main.go
@@ -1,13 +1,19 @@
 package main
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+)
 
 func main() {
+	const filePathRoot = "."
+	const port = "8080"
 	serveMux := http.NewServeMux()
-	serveMux.Handle("/", http.FileServer(http.Dir(".")))
+	serveMux.Handle("/", http.FileServer(http.Dir(filePathRoot)))
 	server := http.Server{
-		Addr:    ":8080",
+		Addr:    ":" + port,
 		Handler: serveMux,
 	}
-	server.ListenAndServe()
+	log.Printf("Serving files from %s on port: %s\n", filePathRoot, port)
+	log.Fatal(server.ListenAndServe())
 }
