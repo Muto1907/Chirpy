d637a8386fef42756393c5cfdd4b811928d19f32
Muto1907 <Mahmut.Cinar93@outlook.de>
2025-03-03 22:33:16 +0100

    Add ready endpoint Handler

--- main.go
+++ main.go
@@ -7,13 +7,22 @@ import (
 
 func main() {
 	const filePathRoot = "."
+	const readinessPath = "/healthz"
 	const port = "8080"
 	serveMux := http.NewServeMux()
-	serveMux.Handle("/", http.FileServer(http.Dir(filePathRoot)))
+	serveMux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filePathRoot))))
+	serveMux.HandleFunc(readinessPath, handler)
 	server := &http.Server{
 		Addr:    ":" + port,
 		Handler: serveMux,
 	}
 	log.Printf("Serving files from %s on port: %s\n", filePathRoot, port)
 	log.Fatal(server.ListenAndServe())
+
+}
+
+func handler(w http.ResponseWriter, req *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte{'O', 'K'})
 }
