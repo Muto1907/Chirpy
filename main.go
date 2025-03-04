66da5d3ab028b163d43b3a98dde0019586470f01
Muto1907 <Mahmut.Cinar93@outlook.de>
2025-03-04 23:11:53 +0100

    Add Routing

--- main.go
+++ main.go
@@ -15,9 +15,9 @@ func main() {
 	}
 	serveMux := http.NewServeMux()
 	serveMux.Handle("/app/", http.StripPrefix("/app", cfg.middlewareMetricsInc(http.FileServer(http.Dir(filePathRoot)))))
-	serveMux.HandleFunc("/metrics", cfg.metricsHandler)
-	serveMux.HandleFunc("/healthz", handlerReady)
-	serveMux.HandleFunc("/reset", cfg.resetHandler)
+	serveMux.HandleFunc("GET /metrics", cfg.metricsHandler)
+	serveMux.HandleFunc("GET /healthz", handlerReady)
+	serveMux.HandleFunc("POST /reset", cfg.resetHandler)
 	server := &http.Server{
 		Addr:    ":" + port,
 		Handler: serveMux,
@@ -52,6 +52,5 @@ func (cfg *apiConfig) metricsHandler(w http.ResponseWriter, req *http.Request) {
 
 func (cfg *apiConfig) resetHandler(w http.ResponseWriter, req *http.Request) {
 	cfg.fileServerHits.Store(0)
-
 	w.WriteHeader(http.StatusOK)
 }
