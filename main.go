1d3f08e9144d04d1241aa51a58471641f0dfdbc7
Muto1907 <Mahmut.Cinar93@outlook.de>
2025-03-05 21:13:09 +0100

    Extract Handlers to own files

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 	"sync/atomic"
@@ -27,38 +26,6 @@ func main() {
 
 }
 
-func handlerReady(w http.ResponseWriter, req *http.Request) {
-	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte{'O', 'K'})
-}
-
 type apiConfig struct {
 	fileServerHits atomic.Int32
-}
-
-func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
-		cfg.fileServerHits.Add(1)
-		next.ServeHTTP(w, req)
-	})
-}
-
-func (cfg *apiConfig) metricsHandler(w http.ResponseWriter, req *http.Request) {
-	w.Header().Add("Content-Type", "text/html; charset=utf-8")
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(fmt.Sprintf(
-		`<html>
-  			<body>
-    			<h1>Welcome, Chirpy Admin</h1>
-    			<p>Chirpy has been visited %d times!</p>
- 			 </body>
-		</html>`,
-		cfg.fileServerHits.Load())),
-	)
-}
-
-func (cfg *apiConfig) resetHandler(w http.ResponseWriter, req *http.Request) {
-	cfg.fileServerHits.Store(0)
-	w.WriteHeader(http.StatusOK)
 }
