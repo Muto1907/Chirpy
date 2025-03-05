660893551400e8a92d6a378bbb2071d4b648f951
Muto1907 <Mahmut.Cinar93@outlook.de>
2025-03-05 21:08:59 +0100

    Change route of metrics and reset to /admin

--- main.go
+++ main.go
@@ -15,9 +15,9 @@ func main() {
 	}
 	serveMux := http.NewServeMux()
 	serveMux.Handle("/app/", http.StripPrefix("/app", cfg.middlewareMetricsInc(http.FileServer(http.Dir(filePathRoot)))))
-	serveMux.HandleFunc("GET /api/metrics", cfg.metricsHandler)
+	serveMux.HandleFunc("GET /admin/metrics", cfg.metricsHandler)
 	serveMux.HandleFunc("GET /api/healthz", handlerReady)
-	serveMux.HandleFunc("POST /api/reset", cfg.resetHandler)
+	serveMux.HandleFunc("POST /admin/reset", cfg.resetHandler)
 	server := &http.Server{
 		Addr:    ":" + port,
 		Handler: serveMux,
@@ -45,9 +45,17 @@ func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
 }
 
 func (cfg *apiConfig) metricsHandler(w http.ResponseWriter, req *http.Request) {
-	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.Header().Add("Content-Type", "text/html; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(fmt.Sprintf("Hits: %d", cfg.fileServerHits.Load())))
+	w.Write([]byte(fmt.Sprintf(
+		`<html>
+  			<body>
+    			<h1>Welcome, Chirpy Admin</h1>
+    			<p>Chirpy has been visited %d times!</p>
+ 			 </body>
+		</html>`,
+		cfg.fileServerHits.Load())),
+	)
 }
 
 func (cfg *apiConfig) resetHandler(w http.ResponseWriter, req *http.Request) {
