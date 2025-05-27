4666a82e24dde3d0d8e907adc04c92dd172ded23
Muto1907 <Mahmut.Cinar93@outlook.de>
2025-05-27 19:25:03 +0200

    Implement sorting Chirps

--- handlerGetChirps.go
+++ handlerGetChirps.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"net/http"
+	"sort"
 
 	"github.com/Muto1907/Chirpy/internal/database"
 	"github.com/google/uuid"
@@ -11,18 +12,19 @@ import (
 func (cfg *apiConfig) getChirps(w http.ResponseWriter, r *http.Request) {
 
 	result := []Chirp{}
-	query_param := r.URL.Query().Get("author_id")
+	query_param_author := r.URL.Query().Get("author_id")
+	query_param_sort_order := r.URL.Query().Get("sort")
 	var chirpsDb []database.Chirp
 	var err error
-	if query_param == "" {
+	if query_param_author == "" {
 		chirpsDb, err = cfg.queries.GetChirps(r.Context())
 		if err != nil {
 			respondWithError(w, 500, "Couldn't get Chirps", err)
 		}
 	} else {
-		author_id, err := uuid.Parse(query_param)
+		author_id, err := uuid.Parse(query_param_author)
 		if err != nil {
-			respondWithError(w, 500, "Couldn't parse author_id", err)
+			respondWithError(w, http.StatusBadRequest, "Couldn't parse author_id", err)
 		}
 		chirpsDb, err = cfg.queries.GetChirpsByAuthor(context.Background(), author_id)
 		if err != nil {
@@ -38,6 +40,12 @@ func (cfg *apiConfig) getChirps(w http.ResponseWriter, r *http.Request) {
 			Body:      chirp.Body,
 			UserId:    chirp.UserID,
 		})
+	}
+	if query_param_sort_order == "desc" {
+		sort.Slice(result,
+			func(i int, j int) bool {
+				return result[i].CreatedAt.After(result[j].CreatedAt)
+			})
 	}
 	respondWithJSON(w, 200, result)
 }
